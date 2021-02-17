9d76b9dea5285ab79e4130f18a6ace3a7b7799b5
Mai Trong Nhan <maitrongnhan.001@gmail.com>
2021-02-17 19:42:06 +0700

    Add by Mai Trong Nhan

---
+++ public/js/Home-Page.js
@@ -0,0 +1,38 @@
+$(".li1").hover(function () {
+        // over
+        $(".li1").css({
+            'background' : '#009933'
+        });
+    }, function () {
+        // out
+        $(".li1").css({
+            'background' : '#336633'
+        });
+    }
+);
+
+$(".li2").hover(function () {
+    // over
+    $(".li2").css({
+        'background' : '#009933'
+    });
+}, function () {
+    // out
+    $(".li2").css({
+        'background' : '#336633'
+    });
+}
+);
+
+$(".li3").hover(function () {
+    // over
+    $(".li3").css({
+        'background' : '#009933'
+    });
+}, function () {
+    // out
+    $(".li3").css({
+        'background' : '#336633'
+    });
+}
+);
