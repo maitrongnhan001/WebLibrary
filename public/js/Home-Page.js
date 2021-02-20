e71ac3245747a00d6497e2c916766a1514cdd164
maitrongnhan001 <maitrongnhan.001@student.ctu.edu.vn>
2021-02-20 08:44:31 +0700

    Add by Mai Trong Nhan

--- public/js/Home-Page.js
+++ public/js/Home-Page.js
@@ -40,6 +40,49 @@ $(".li3").hover(function () {
 
 /*----------------------------------------------*/
 
-/*-----------------hover product----------------*/
 
+
+/*---------------Click button get---------------*/
+$(".main .btn").click(function (e) { 
+    e.preventDefault();
+    $(".menu").css({"display" : "none"});
+    $(".header").css({"display" : "none"});
+    $(".main").css({"display" : "none"});
+    $(".end").css({"display" : "none"});
+    $("body").addClass("getBody");
+    $(".formGetBook").css({"display" : "block"});
+});
+
+$(".cancel").click(function (e) { 
+    e.preventDefault();
+    $(".menu").css({"display" : "block"});
+    $(".header").css({"display" : "block"});
+    $(".main").css({"display" : "block"});
+    $(".end").css({"display" : "flex"});
+    $("body").removeClass("getBody");
+    $(".formGetBook").css({"display" : "none"});
+    $(".formReturnBook").css({"display" : "none"});
+});
+/*----------------------------------------------*/
+
+/*-------------------Click menu-----------------*/
+$(".li1").click(function (e) { 
+    e.preventDefault();
+    $(".menu").css({"display" : "none"});
+    $(".header").css({"display" : "none"});
+    $(".main").css({"display" : "none"});
+    $(".end").css({"display" : "none"});
+    $("body").addClass("getBody");
+    $(".formGetBook").css({"display" : "block"});
+});
+
+$(".li2").click(function (e) { 
+    e.preventDefault();
+    $(".menu").css({"display" : "none"});
+    $(".header").css({"display" : "none"});
+    $(".main").css({"display" : "none"});
+    $(".end").css({"display" : "none"});
+    $("body").addClass("getBody");
+    $(".formReturnBook").css({"display" : "block"});
+});
 /*----------------------------------------------*/
